2f213e4f0a3edb34f4deea1a4c31e8c7209cd365
Jesse Griffin <jesse.griffin@gmail.com>
2020-02-14 06:12:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,7 +34,11 @@ class TicTacToe
    end 
    
    def position_taken(index) 
-      
+      if @board[index] =="X" || @board[index] == "O"
+        true 
+      else 
+        false 
+      end 
    end 
    
    
